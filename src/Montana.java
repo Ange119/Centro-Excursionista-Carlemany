7fedb9a4603195996203883c491b1d1d15c8a0d8
Angelhome <amoreyra732@gmail.com>
2024-12-02 21:02:44 +0100

    agregacion Montaña y Expedicion

---
+++ src/Montana.java
@@ -0,0 +1,13 @@
+public class Montana {
+    private String nombre ;
+    private int altitud;
+    private String ubicacion;
+
+    //Constructor
+    public Montana(String nombre , int altitud, String ubicacion) {
+        this.nombre = nombre; // aqui llamo a la variable nombre y que se aguarde en nombre del constructor
+        this.altitud = altitud;
+        this.ubicacion = ubicacion;
+    }
+
+}
